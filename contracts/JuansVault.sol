dc2fed53b00286e715e6d30280ad7d3ef5595717
0xjuan <juan.0x@outlook.com>
2022-01-15 12:33:26 -0500

    juan/update: JuansVault

--- contracts/JuansVault.sol
+++ contracts/JuansVault.sol
@@ -4,23 +4,36 @@ pragma solidity ^0.7.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract JuansVault {
-    address[] erc20_tokens;
+    address public owner;
+    address[] tokens_erc20;
 
-    function balanceOf(address erc20_token)
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function balanceOf(address token_erc20)
         public
         view
         virtual
         returns (uint256)
     {
-        return IERC20(erc20_token).balanceOf(address(this));
+        return IERC20(token_erc20).balanceOf(address(this));
+    }
+
+    function deposit(address token_erc20, uint256 amount)
+        public
+        returns (bool)
+    {
+        IERC20(token_erc20).transferFrom(owner, address(this), amount);
+        return true;
     }
 
     function withdraw(
-        address erc20_token,
+        address token_erc20,
         address recipient,
         uint256 amount
     ) public returns (bool) {
-        IERC20(erc20_token).transfer(recipient, amount);
+        IERC20(token_erc20).transfer(recipient, amount);
         return true;
     }
 }
--- test/JuansVault.js
+++ test/JuansVault.js
@@ -1,6 +1,10 @@
 const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('JuansVault contract', function () {
+    let initialBalance;
+    let USDC;
+    let hardhatUSDC;
     let JuansVault;
     let hardhatJuansVault;
     let owner;
@@ -9,31 +13,40 @@ describe('JuansVault contract', function () {
     let addrs;
 
     beforeEach(async function () {
-        // Get the ContractFactory and Signers here.
+        USDC = await ethers.getContractFactory('USDC');
         JuansVault = await ethers.getContractFactory('JuansVault');
         [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
 
-        // To deploy our contract, we just have to call Token.deploy() and await
-        // for it to be deployed(), which happens onces its transaction has been
-        // mined.
+        initialBalance = 1000000;
+
+        hardhatUSDC = await USDC.deploy(initialBalance);
         hardhatJuansVault = await JuansVault.deploy();
 
-        // We can interact with the contract by calling `hardhatToken.method()`
+        await hardhatUSDC.deployed();
         await hardhatJuansVault.deployed();
     });
 
     describe('Deployment', function () {
-        // `it` is another Mocha function. This is the one you use to define your
-        // tests. It receives the test name, and a callback function.
+        it('owner should have a balance of 1,000,000 USDC', async function () {
+            expect(await hardhatUSDC.balanceOf(owner.address)).to.equal(initialBalance);
+        });
 
-        // If the callback function is async, Mocha will `await` it.
         it('Should set the right owner', async function () {
-            // Expect receives a value, and wraps it in an assertion objet. These
-            // objects have a lot of utility methods to assert values.
-
-            // This test expects the owner variable stored in the contract to be equal
-            // to our Signer's owner.
             expect(await hardhatJuansVault.owner()).to.equal(owner.address);
+        });
+    });
+
+    describe('Transactions', () => {
+        it('deposit 10 USDC to vault', async () => {
+            transferAmount = 10;
+
+            await hardhatUSDC.approve(hardhatJuansVault.address, transferAmount);
+            await hardhatJuansVault.deposit(hardhatUSDC.address, transferAmount);
+
+            let remainingBalance = initialBalance - transferAmount;
+
+            expect(await hardhatUSDC.balanceOf(owner.address)).to.equal(remainingBalance);
+            expect(await hardhatUSDC.balanceOf(hardhatJuansVault.address)).to.equal(transferAmount);
         });
     });
 });
